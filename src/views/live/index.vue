db61a974e856998a7cb29cc1ff1955bec0269cf8
Gujuntao <nicemegu@163.com>
2021-10-28 10:20:59 +0800

    feta  更新代码

--- src/views/live/index.vue
+++ src/views/live/index.vue
@@ -1,46 +1,83 @@
 <template>
   <div>
-    <div class="testimg" >
-      <img src="https://w.wallhaven.cc/full/8o/wallhaven-8oky1j.jpg" alt="">
-    </div>
+    <video :id="tcPlayerId" class="tencent-player" preload="auto" playsinline webkit-playsinline></video>
   </div>
 </template>
-
 <script>
-// import { request } from "../../api/text"
-export default {
-  data () {
-    return {
-    }
-  },
-
-  components: {},
-
-  computed: {},
-
-  mounted () {
-    //  request({
-    //     url: 'api/weibo/'
-    //   }).then(res => {
-    //     this.newList = res.data
-    //   }, err => {
-    //     console.log(err);
-    //   })
-  },
-
-  methods: {
-
+function loadTcScript (cb) {
+  loadScript(cb, {
+    id: 'tcPlayerScriptId',
+    url: '//imgcache.qq.com/open/qcloud/video/tcplayer/tcplayer.min.js',
+  });
+}
+function loadScript (cb, obj) {
+  if (document.getElementById(obj.id)) {
+    cb();
+  } else {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = obj.url;
+    script.id = obj.id;
+    script.onload = () => {
+      cb();
+    };
+    document.body.appendChild(script);
   }
 }
-
+export default {
+  name: 'TencentPlayer',
+  props: {
+    options: {
+      type: Object,
+      default () {
+        return {};
+      }
+    }
+  },
+  data () {
+    return {
+      tcPlayerId: 'tcPlayer' + Date.now(),
+      player: null,
+    };
+  },
+  watch: {
+    options: {
+      handler () {
+        this.$nextTick(() => {
+          this.loadJS();
+        });
+      },
+      immediate: true,
+      deep: true,
+    }
+  },
+  methods: {
+    loadJS () {
+      if (window.TCPlayer) {
+        this.initVideo();
+      } else {
+        loadTcScript(() => {
+          this.initVideo();
+        });
+      }
+    },
+    initVideo () {
+      const playerParm = {
+        fileID: '5285890799710670616',
+        appID: '1400329073',
+        autoplay: this.options.autoplay ? true : false,
+      };
+      setTimeout(() => {
+        if (!this.player) {
+          // this.player = TCPlayer(this.tcPlayerId, playerParm);
+        } else {
+          this.player.loadVideoByID(playerParm);
+        }
+      });
+    }
+  },
+};
 </script>
 <style  scoped>
-.testimg {
-  width: 200px;
-  height: 200px;
-}
-.testimg > img {
-  width: 100%;
-  height: 100%;
-}
+@import url('//imgcache.qq.com/open/qcloud/video/tcplayer/tcplayer.css');
 </style>
